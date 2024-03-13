b8ba85fc4e8bb491db87199878d80dab136a6f3b
Dmitry <dmitrylancer.it@yandex.ru>
2024-03-13 19:35:28 +0300

    контроллер и вьюшка для загрузки изображений

---
+++ resources/views/images/upload.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Upload Images</title></head>
+<body>
+
+<form action="/upload" method="post" enctype="multipart/form-data">
+    @csrf
+    <input type="file" name="image">
+    <button type="submit">Загрузить</button>
+</form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/upload', [App\Http\Controllers\ImageController::class, 'showUploadForm']);
+Route::post('/upload', [App\Http\Controllers\Controller::class, 'uploadImage']);
+
+
